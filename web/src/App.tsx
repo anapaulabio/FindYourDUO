1d8b177a342c3a62eef848828065bd868cffd24c
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-09-15 16:50:36 -0300

    setting langing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,70 @@
+import './styles/main.css'
 
 function App() {
-  return <h1>Hello Word!</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex items-center flex-col m-20">
+      <img src="/logo-nlw-landing.png" alt="" />
+      
+      <h1 className="text-6xl text-white font-black m-20" >Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.</h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-4">
+          <a className="relative rounded-lg" href="">
+          <img src="/game-06.png" alt="" />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+       
+        </a>
+        <a className="relative" href="">
+          <img src="/game-01.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legend</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative" href="">
+          <img src="game-02.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative" href="">
+          <img src="game-03.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative" href="">
+          <img src="game-04.png" alt="" />
+        
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a className="relative" href="">
+          <img src="game-05.png" alt="" />
+        
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="bg-[#2A2634] px-8 py-6 mt-8 self-stretch rounded-lg ">
+
+      </div>
+
+    </div>
+  )
 
 }
 
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        background: "url('/galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 10.00%, #43E7AD 50.00%, #E1D55D 90%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    },
+  },
+  plugins: [],
+}
