00435b8debdac1faf6f53e777421d0b47f8b70fb
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-09-17 19:46:03 -0300

    server integration with mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
 import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://192.168.0.102:3000/games')
+    .then(res => res.json())
+    .then(data => setGames(data))
+    }, []);
+
+
   return (
     <View style={styles.container}>
       <Image 
@@ -19,12 +29,12 @@ export function Home() {
       subtitle="Selecione o game que deseja jogar..."/>
 
       <FlatList 
-      data={GAMES}
+      data={games}
       keyExtractor={item => item.id}
       renderItem={({ item }) => (
         <GameCard data={item} />
       )}
-      showsHorizontalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false} 
       horizontal
       contentContainerStyle={styles.contentList}
       />
