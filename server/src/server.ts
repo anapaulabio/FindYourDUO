face272d0aa756a9806fbd11f719e7f219c3fdc1
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-09-16 22:30:07 -0300

    setting server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,83 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+
+const prisma = new PrismaClient();
 
 const app = express();
+
+app.use(express.json());
+
+
+app.get('/games', async (req, res) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true
+                }
+            }
+        }
+    });
+    res.json(games); 
+});
+
+app.get('/games/:id/ads', async (req, res) => {
+    const gameId = req.params.id
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true
+        },
+        where: {
+            gameId
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    });
+    return res.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }));
+});
+
+app.get('/games/:id/discord',async (req, res) => {
+    const adId = req.params.id;
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord:true,
+        },
+        where: {
+            id: adId,
+        }
+    })
+    res.json({discord: ad.discord});
+});
+
+app.post('/games/:id/ads', async (req, res) => {
+    const gameId = req.params.id;
+    const body:any = req.body;
+
+    const newAd = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays,
+            hourStart: body.hourStart,
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+    res.status(201).json(newAd) 
+});
 
 
 app.listen(3000, () => 
